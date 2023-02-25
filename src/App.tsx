0f976d9d0581a2f44334a848c32e60f467fb44ab
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-25 14:07:31 +0300

    LessonButton.tsx oluşturuldu ve import edildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import LessonTypography from './Components/LessonTypography';
+import { LessonButton } from './Components/LessonButton';
 
 function App() {
   return (
     <div className="App">
-     <LessonTypography/>
+      {/* <LessonTypography/> */}
+      <LessonButton />
     </div>
   );
 }
