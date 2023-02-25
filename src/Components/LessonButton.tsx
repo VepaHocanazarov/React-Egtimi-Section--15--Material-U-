d7f3033ab988dc4ac3e17e43f5dbfa44a7d2dccc
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-25 15:58:49 +0300

    151. Buttonlar

--- src/Components/LessonButton.tsx
+++ src/Components/LessonButton.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Button, Stack } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 export const LessonButton = () => {
-  return (
-    <div>LessonButton</div>
-  )
+    return (
+
+        <Stack spacing={4}>
+            <Stack direction="row" spacing={2}>
+                <Button variant='text' >Text</Button>
+                <Button variant='outlined' >Outline</Button>
+                <Button variant='contained' >Contained</Button>
+            </Stack>
+
+            <Stack direction="row" spacing={2}>
+                <Button variant='text' color='primary' >Text</Button>
+                <Button variant='text' color='secondary' >Mor</Button>
+                <Button variant='text' color='success' >Yeşil</Button>
+                <Button variant='text' color='error' >Kırımızı</Button>
+                <Button variant='text' color='warning' >Sarı</Button>
+                <Button variant='text' color='info' >Mavi</Button>
+            </Stack>
+
+            <Stack direction="row" spacing={2}>
+                <Button variant='outlined' color='primary' >Text</Button>
+                <Button variant='outlined' color='secondary' >Mor</Button>
+                <Button variant='outlined' color='success' >Yeşil</Button>
+                <Button variant='outlined' color='error' >Kırımızı</Button>
+                <Button variant='outlined' color='warning' >Sarı</Button>
+                <Button variant='outlined' color='info' >Mavi</Button>
+            </Stack>
+
+            <Stack direction="row" spacing={2}>
+                <Button variant='contained' color='primary' >Text</Button>
+                <Button variant='contained' color='secondary' >Mor</Button>
+                <Button variant='contained' color='success' >Yeşil</Button>
+                <Button variant='contained' color='error' >Kırımızı</Button>
+                <Button variant='contained' color='warning' >Sarı</Button>
+                <Button variant='contained' color='info' >Mavi</Button>
+            </Stack>
+
+            <Stack direction='row' display='block' spacing={3} >
+                <Button variant='outlined' size='small' >Küçük</Button>
+                <Button variant='outlined' size='medium' >Orta</Button>
+                <Button variant='outlined' size='large' >Büyük</Button>
+            </Stack>
+
+            <Stack direction="row" spacing={2}>
+            <AddIcon color='error'/>
+
+            <Button variant='contained' startIcon={<AddIcon />} >Ekle</Button>
+            <Button variant='contained' endIcon={<AddIcon />} onClick={()=> alert("Tıklandı") }>Ekle</Button>
+
+            </Stack>
+        </Stack>
+
+    )
 }
