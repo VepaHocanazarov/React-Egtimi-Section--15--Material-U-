7c035a6e58a72d4c783abcebfa6b84e1518af311
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-26 08:20:10 +0300

    LessonButtonGroup.tsx oluşturuldu ve İmport edildi

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LessonTypography from './Components/LessonTypography';
 import { LessonButton } from './Components/LessonButton';
+import { ButtonGroup } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       {/* <LessonTypography/> */}
-      <LessonButton />
+      {/* <LessonButton /> */}
+      <ButtonGroup/>
     </div>
   );
 }
---
+++ src/Components/LessonButtonGroup.tsx
@@ -0,0 +1,11 @@
+import { Button, Stack,ButtonGroup } from '@mui/material';
+
+import React from 'react'
+
+export const LessonButtonGroup = () => {
+  return (
+<Stack>
+    
+</Stack>
+  )
+}
