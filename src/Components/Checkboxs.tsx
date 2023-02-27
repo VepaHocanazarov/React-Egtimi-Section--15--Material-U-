1a8897f557238b773fc78a283e7053fa9ab53e87
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-27 15:58:58 +0300

    158. Chekckbox

--- src/Components/Checkboxs.tsx
+++ src/Components/Checkboxs.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { FormControl, FormLabel, FormControlLabel, Box, Checkbox, FormHelperText, FormGroup } from "@mui/material";
+import { useState } from 'react';
 
 export const Checkboxs = () => {
+
+  const [isAcsept,setIsAcsept] = useState(false);
+  console.log(isAcsept);
+  
   return (
-    <div>Checkbox</div>
+
+    <Box>
+
+      <Box>
+        <FormControl>
+          <FormLabel>Egtim Durumunuzu Seçiniz</FormLabel>
+          <FormGroup row >
+
+            <FormControlLabel disabled control={<Checkbox />} label="İlkokul" />
+            <FormControlLabel control={<Checkbox />} label="Lise" />
+            <FormControlLabel control={<Checkbox defaultChecked />} label="Universite" />
+
+          </FormGroup>
+        </FormControl>
+      </Box>
+
+      <Box>
+        <FormControl>
+          <FormLabel>Egtim Durumunuzu Seçiniz</FormLabel>
+          <FormGroup row >
+
+            <FormControlLabel  control={<Checkbox checked={isAcsept}
+             onChange={(e)=>setIsAcsept(e.target.checked)} />}
+             label="Kullanım Koşullarını Kabul Ediyourm." />
+
+          </FormGroup>
+        </FormControl>
+      </Box>
+    </Box>
+
   )
 }
