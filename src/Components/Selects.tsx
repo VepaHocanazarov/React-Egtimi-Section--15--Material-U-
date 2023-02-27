56528362fd5f6044a46c4eb2252d2c71ed8a76ed
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-27 13:24:43 +0300

    157. Select Çoklu Seçim

--- src/Components/Selects.tsx
+++ src/Components/Selects.tsx
@@ -3,13 +3,20 @@ import React from 'react';
 import { useState } from 'react';
 
 export const Selects = () => {
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState<string[]>([]);
     console.log(value);
     
     return (
         <Box width="200px">
 
-            <TextField select label="Ülke Seçniz" fullWidth value={value} onChange={(e)=>setValue(e.target.value)} >
+            <TextField select label="Ülke Seçniz" fullWidth value={value}
+             onChange={(e)=>setValue( typeof e.target.value === "string"
+              ? e.target.value.split(",")
+             : e.target.value
+             )}
+             SelectProps={{
+                multiple:true
+             }} >
 
                 <MenuItem value="TR"> Türkiye</MenuItem>
                 <MenuItem value="USE"> ABD</MenuItem>
