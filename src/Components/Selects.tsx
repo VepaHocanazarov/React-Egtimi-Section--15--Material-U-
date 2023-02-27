610945538d06fe4496b07f3937af59d6f44e6a74
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-27 12:30:23 +0300

    156. Select

--- src/Components/Selects.tsx
+++ src/Components/Selects.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Box, TextField, MenuItem } from '@mui/material'
+import React from 'react';
+import { useState } from 'react';
 
 export const Selects = () => {
-  return (
-    <div>Selects</div>
-  )
-}
+    const [value, setValue] = useState("");
+    console.log(value);
+    
+    return (
+        <Box width="200px">
+
+            <TextField select label="Ülke Seçniz" fullWidth value={value} onChange={(e)=>setValue(e.target.value)} >
+
+                <MenuItem value="TR"> Türkiye</MenuItem>
+                <MenuItem value="USE"> ABD</MenuItem>
+                <MenuItem value="FR"> FRANCE</MenuItem>
+
+            </TextField>
+
+        </Box>
+    );
+};
