5f1880ada989152cb733a54a725b9d10c52a0e8a
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-27 10:05:33 +0300

    155. Radio Group ufak bir örnek

--- src/Components/RadioExample.tsx
+++ src/Components/RadioExample.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { FormControl, FormLabel, FormControlLabel, Radio, RadioGroup, Button, FormHelperText } from "@mui/material";
+import { useState } from 'react';
 
 export const RadioExample = () => {
-  return (
-    <div>RadioExample</div>
-  )
+
+    const [value, setvalue] = useState("");
+    const [helpertext, sethelpertext] = useState("");
+    const [error, seterror] = useState(false);
+
+    const handleChange = (event:React.ChangeEvent<HTMLInputElement>) =>{
+       setvalue(event.target.value);
+    }
+
+    const handleSubmit = (event:React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+        if (value === "canboz"){
+            sethelpertext("Dodru Cevap Ofcourse Can Boz Olacak!");     
+            seterror(true)   
+        }
+
+        else if(value === "ofCourseCanBoz"){
+            sethelpertext("Bravo Dogru Seçim!");
+            seterror(false)
+        }
+
+        else{
+            sethelpertext("Lütfen bir seçim yapınız");
+            seterror(true)
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit} >
+            <FormControl error={error} >
+                <FormLabel>En İyi Frontend Egitmeni Seçiniz</FormLabel>
+                <RadioGroup onChange={handleChange} >
+                    <FormControlLabel label="Can Boz" value="canboz" control={<Radio />} />
+                    <FormControlLabel label=" Ofcourse Can Boz" value="ofCourseCanBoz" control={<Radio />} />
+                </RadioGroup>
+                <FormHelperText>{helpertext}</FormHelperText>
+                <Button type='submit' variant='outlined' >Ekle</Button>
+            </FormControl>
+        </form>
+    )
 }
