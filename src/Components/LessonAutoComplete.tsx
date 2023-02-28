1982ef0ba5ccd3df251a73eebdb631b98ce6b976
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-28 11:41:14 +0300

    160. AutoComplete

--- src/Components/LessonAutoComplete.tsx
+++ src/Components/LessonAutoComplete.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { TextField, Autocomplete } from '@mui/material';
+
+const top100Films = [
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "God Father", year: 1994 },
+  { label: "God Father", year: 1994 },
+  { label: "God Father", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "TheRobin Hood", year: 1994 },
+  { label: "TheRobin Hood", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The vandam", year: 1994 },
+  { label: "The vandam", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Dark night", year: 1994 },
+  { label: "The Dark night", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Fight Club", year: 1994 },
+  { label: "The Fight Club", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Avangers", year: 1994 },
+  { label: "The Avangers", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "Ant Man 1", year: 1994 },
+  { label: "Ant Man 1", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Shawsnak Redemtion", year: 1994 },
+  { label: "The Wonder Woman", year: 1994 },
+
+];
 
 export const LessonAutoComplete = () => {
   return (
-    <div>LessonAutoComplete</div>
+    <Autocomplete
+      options={top100Films}
+      sx={{width:300}}
+      renderInput={(params) => <TextField {...params} label="Filmler" />}
+    />
   )
 }
