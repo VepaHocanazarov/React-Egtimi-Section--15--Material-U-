d9987f263f700e53e58f0c7beac59c0c9b8b5625
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-28 09:24:22 +0300

    159. Chekckbox Çoklu Seçim ufak proje

--- src/Components/Checkboxs.tsx
+++ src/Components/Checkboxs.tsx
@@ -4,9 +4,24 @@ import { useState } from 'react';
 
 export const Checkboxs = () => {
 
-  const [isAcsept,setIsAcsept] = useState(false);
+  const [isAcsept, setIsAcsept] = useState(false);
+  const [knowlegde, setKnowlegde] = useState<string[]>([]);
+
+  console.log(knowlegde)
+
+  const handleKnowlegdeChange = (event:React.ChangeEvent<HTMLInputElement>) =>{
+    const index = knowlegde.indexOf(event.target.value);
+    if(index === -1){
+      setKnowlegde([...knowlegde,event.target.value]);
+    }
+    else{
+      setKnowlegde(knowlegde.filter((item)=>item !== event.target.value));
+    }
+
+  }
+
   console.log(isAcsept);
-  
+
   return (
 
     <Box>
@@ -29,9 +44,33 @@ export const Checkboxs = () => {
           <FormLabel>Egtim Durumunuzu Seçiniz</FormLabel>
           <FormGroup row >
 
-            <FormControlLabel  control={<Checkbox checked={isAcsept}
-             onChange={(e)=>setIsAcsept(e.target.checked)} />}
-             label="Kullanım Koşullarını Kabul Ediyourm." />
+            <FormControlLabel control={<Checkbox checked={isAcsept}
+              onChange={(e) => setIsAcsept(e.target.checked)} />}
+              label="Kullanım Koşullarını Kabul Ediyourm." />
+
+          </FormGroup>
+        </FormControl>
+      </Box>
+
+      <Box>
+        <FormControl>
+          <FormLabel error >Frontend de Neleri biliyorsunuz</FormLabel>
+          <FormGroup row >
+
+            <FormControlLabel control={<Checkbox checked={knowlegde.includes("react")}
+              onChange={handleKnowlegdeChange} />}
+              label="React Js"
+              value="react" />
+
+            <FormControlLabel control={<Checkbox checked={knowlegde.includes("angular")}
+              onChange={handleKnowlegdeChange} />}
+              label="Angular Js"
+              value="angular" />
+
+            <FormControlLabel control={<Checkbox checked={knowlegde.includes("vue")}
+              onChange={handleKnowlegdeChange} />}
+              label="Vue Js"
+              value="vue" />
 
           </FormGroup>
         </FormControl>
