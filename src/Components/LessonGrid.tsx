c2dbff42f9a4b28e4079f3c62ff03d7f4a0ba035
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-01 08:40:07 +0300

    163. Grid anlatımı

--- src/Components/LessonGrid.tsx
+++ src/Components/LessonGrid.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid,Box } from '@mui/material';
 
 export const LessonGrid = () => {
-  return (
-    <Grid>LessonGrid</Grid>
-  )
+    return (
+
+        <Box>
+            <Grid container >
+                <Grid xs={12} md={8} >8 birmlik yer</Grid>
+                <Grid xs={12} md={4}>4 birmlik yer</Grid>
+                <Grid xs={12} md={8}>8 birmlik yer</Grid>
+                <Grid xs={12} md={4}>4 birmlik yer</Grid>
+            </Grid>
+
+            {/* <Grid container >
+                <Grid xs={8} >8 birmlik yer</Grid>
+                <Grid xs={4} >4 birmlik yer</Grid>
+            </Grid> */}
+        </Box>
+
+    )
 }
