00305b97aa408dec1815a341eab0e5221cc25b9c
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-01 16:09:59 +0300

    166. AppBar Kullanımı

--- src/Components/LessonAppBar.tsx
+++ src/Components/LessonAppBar.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, Menu,MenuItem,IconButton,Typography,Stack, Button } from '@mui/material';
+import AppsIcon from '@mui/icons-material/Apps';
+import { useState } from 'react';
 
 export const LessonAppBar = () => {
+
+    const  [anchorEl, setAnchorEl] = useState <null | HTMLElement>(null);
+    const openControl = Boolean(anchorEl);
+    console.log(openControl);
+
+    const handleClick = (event:React.MouseEvent<HTMLButtonElement>)=>{
+        setAnchorEl(event.currentTarget);
+        console.log(event.currentTarget);
+        
+    };
+
+    const handleClose = ()=>{
+        setAnchorEl(null)
+    }
   return (
-    <div>LessonAppBar</div>
+    <AppBar>
+        <Toolbar>
+
+            <IconButton>
+                <AppsIcon/>
+            </IconButton>
+
+            <Typography variant='h6' component="div" sx={{marginRight:"auto"}}>
+                Header Örnegi
+            </Typography>
+
+            <Stack direction="row"  >
+                <Button sx={{color:"white"}}>Anasayfa</Button>
+                <Button sx={{color:"white"}}>Hakkımıza</Button>
+                <Button sx={{color:"white"}}>Ürünler</Button>
+                <Button sx={{color:"white"}}>Fiyatlar</Button>
+                <Button sx={{color:"white"}}>Fiyatlar</Button>
+                <Button sx={{color:"white"}} onClick={handleClick} >Indirimdekiler</Button>
+
+            </Stack>
+
+            <Menu anchorEl={anchorEl} open={openControl} onClose={handleClose} >
+                <MenuItem onClick={handleClose} >Kitaplar</MenuItem>
+                <MenuItem  onClick={handleClose}>Defterler</MenuItem>
+                <MenuItem  onClick={handleClose}>Kalemler</MenuItem>
+
+            </Menu>
+            
+        </Toolbar>
+    </AppBar>
   )
 }
