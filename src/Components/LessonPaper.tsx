8970420e6f176a03e17d88c9dd089c16e47f2377
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-02 11:43:50 +0300

    168. Paper Kullanımı

--- src/Components/LessonPaper.tsx
+++ src/Components/LessonPaper.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Paper,Box } from '@mui/material';
 
 export const LessonPaper = () => {
   return (
-    <div>LessonPaper</div>
+
+    <Box sx={{display:'flex',gap:"20px"}}  >
+    <Paper sx={{width:100, height:100}} />
+    <Paper sx={{width:100, height:100}} elevation={3} />
+    <Paper sx={{width:100, height:100}} elevation={2} />
+    <Paper sx={{width:100, height:100}} variant='outlined' square />
+
+
+
+    </Box>
   )
 }
