36d8cb65dc45cc02e342483c62bd2cd139c082ae
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-02 16:37:18 +0300

    172. Badge Kullanımı

--- src/Components/LessonBadge.tsx
+++ src/Components/LessonBadge.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Stack,Badge} from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
 
 export const LessonBadge = () => {
   return (
-    <div>LessonBadge</div>
-  )
+<Stack direction="row" spacing={3}>
+    <Badge badgeContent={50} color='secondary'>
+        <MailIcon />
+    </Badge>
+
+    <Badge badgeContent={50} color='secondary' max={40}>
+        <MailIcon />
+    </Badge>
+
+    <Badge badgeContent={50} color='secondary' anchorOrigin={{
+        vertical:"bottom",
+        horizontal:"right"
+    }}>
+        <MailIcon />
+    </Badge>
+
+    <Badge badgeContent={50} color='secondary' anchorOrigin={{
+        vertical:"bottom",
+        horizontal:"left"
+    }}>
+        <MailIcon />
+    </Badge>
+</Stack>
+    )
 }
