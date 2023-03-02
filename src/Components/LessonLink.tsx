ab1ed8c71d7ac1cb597931ba739a3263d6a046f9
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-02 12:30:06 +0300

    169. Link Kullanımı

--- src/Components/LessonLink.tsx
+++ src/Components/LessonLink.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link, Box } from '@mui/material';
 
 export const LessonLink = () => {
-  return (
-    <div>LessonLink</div>
-  )
+    return (
+
+        <Box >
+            {/* <Link href='https://www.canboz.com/' >Websiteme Git</Link>
+            <Link href='https://www.canboz.com/' color="error" >Websiteme Git</Link>
+            <Link href='https://www.canboz.com/' color="error" variant='body2'>Websiteme Git</Link> */}
+
+            <Link href='https://www.canboz.com/' sx={{padding:4}} underline='none'>Websiteme Git</Link>
+            <Link href='https://www.canboz.com/' sx={{padding:4}} underline='hover'>Websiteme Git</Link>
+            <Link href='https://www.canboz.com/' sx={{padding:4}} underline='always' component="button" onClick={()=>{
+                console.log("Vepa");               
+            }} >Websiteme Git </Link>
+
+
+
+        </Box>
+
+
+    )
 }
