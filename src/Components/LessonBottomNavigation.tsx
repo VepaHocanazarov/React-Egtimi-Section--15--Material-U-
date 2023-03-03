82a7afddf9f522d038515a25f8fe3b9e58038d43
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-03 11:25:36 +0300

    174. BottomNavigation Kullanımı

--- src/Components/LessonBottomNavigation.tsx
+++ src/Components/LessonBottomNavigation.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BottomNavigation,BottomNavigationAction } from '@mui/material';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export const LessonBottomNavigation = () => {
+
+const [value, setValue] = useState(0);
+console.log(value);
+
+
   return (
-    <div>LessonBottomNavigation</div>
+    <BottomNavigation showLabels
+    value={value}
+    sx={{position:"absolute",width:"100%",bottom:0}}
+    onChange={(event,newValue)=>{
+        setValue(newValue)
+    }}
+    >
+        <BottomNavigationAction icon={<RestoreIcon/>} label="Öncekiler" />
+        <BottomNavigationAction icon={<FavoriteIcon/>} label="Sevdiklerim" />
+        <BottomNavigationAction icon={<LocationOnIcon/>} label="Konumum" />
+    </BottomNavigation>
   )
 }
