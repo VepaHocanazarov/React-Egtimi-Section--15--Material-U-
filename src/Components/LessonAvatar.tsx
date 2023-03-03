c373d463b366e9623dde1bc8f008cd67c8eeed67
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-03 12:06:41 +0300

    175. Avatar Kullamımı

--- src/Components/LessonAvatar.tsx
+++ src/Components/LessonAvatar.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import { Stack,AvatarGroup,Avatar } from '@mui/material';
 
+
+
 export const LessonAvatar = () => {
   return (
-    <div>LessonAvatar</div>
+    <Stack direction="row" spacing={5} >
+
+        <AvatarGroup>
+            <Avatar sx={{marginRight:10}} src='https://randomuser.me/api/portraits/women/56.jpg' />
+            <Avatar src='https://randomuser.me/api/portraits/men/56.jpg' />
+
+        </AvatarGroup>
+
+        
+        <AvatarGroup max={6}>
+          <Avatar sx={{marginRight:10,bgcolor:"deepskyblue",width:50,height:50,padding:0.1}} >Vepa</Avatar>
+          <Avatar sx={{marginRight:10,bgcolor:"deepskyblue",width:50,height:50,padding:0.1}} variant='square'>Aygul</Avatar>
+          <Avatar sx={{marginRight:10,bgcolor:"deepskyblue",width:50,height:50,padding:0.1}} >Vepa</Avatar>
+          <Avatar sx={{marginRight:10,bgcolor:"deepskyblue",width:50,height:50,padding:0.1}}  >Aygul</Avatar>  
+          <Avatar sx={{marginRight:10,bgcolor:"deepskyblue",width:50,height:50,padding:0.1}}  >Aygul</Avatar>
+          <Avatar sx={{marginRight:10,bgcolor:"deepskyblue",width:50,height:50,padding:0.1}} >Vepa</Avatar>
+
+        </AvatarGroup>
+        
+    </Stack>
   )
 }
