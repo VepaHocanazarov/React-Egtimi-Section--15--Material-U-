167338101f6aacfb867f7f4a1024f08f00480136
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-03 15:43:35 +0300

    178. Alert Kullanımı

--- src/Components/LessonAlert.tsx
+++ src/Components/LessonAlert.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Alert,Stack,AlertTitle} from '@mui/material';
+import { Alert, Stack, AlertTitle } from '@mui/material';
 
 export const LessonAlert = () => {
-  return (
-    <div>LessonAlert</div>
-  )
+    return (
+
+        <Stack spacing={2} >
+
+            <Alert severity='error'>
+                <AlertTitle>Error</AlertTitle>
+                LessonAlert</Alert>
+            <Alert severity='warning'>
+                <AlertTitle>Error</AlertTitle>
+                LessonAlert</Alert>
+            <Alert severity='success'>
+                <AlertTitle>Error</AlertTitle>
+                LessonAlert</Alert>
+            <Alert severity='info'>
+                <AlertTitle>Error</AlertTitle>
+                LessonAlert</Alert>
+
+        </Stack>
+    )
 }
