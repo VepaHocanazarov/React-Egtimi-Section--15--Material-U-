bb4446939855a39e6a4834b138ca56e9224834ef
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-03 14:56:27 +0300

    177. ToolTip Kullanımı

--- src/Components/LessonToolTip.tsx
+++ src/Components/LessonToolTip.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Tooltip, IconButton} from '@mui/material';
+import { Tooltip, IconButton, Button, Box } from '@mui/material';
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export const LessonToolTip = () => {
-  return (
-    <div>LessonToolTip</div>
-  )
+    return (
+
+        <Box>
+
+            <Tooltip title="Sil" placement='top' >
+                <IconButton sx={{marginRight:"10px"}}>
+                    <DeleteIcon />
+                </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Add"  disableInteractive >
+              <Button  >Arrow</Button>
+            </Tooltip>
+
+            
+            <Tooltip title="Add"  disableInteractive >
+              <Button disabled >Arrow</Button>
+            </Tooltip>
+
+
+        </Box>
+
+    )
 }
