461c20eeeb13a8e739ee82f7f17fd48afb080fa2
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-04 16:15:40 +0300

    182. Skeleton Kullanımı

--- src/Components/LessonSkeleton.tsx
+++ src/Components/LessonSkeleton.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import {Stack,Skeleton,Box} from "@mui/material";
+import {Stack,Skeleton,Box,Button} from "@mui/material";
+import {useState} from "react";
 
 export const LessonSkeleton = () => {
+    const [loading, setLoading] = useState(false)
   return (
-    <Box>
-        
+    <Box >
+
+        {loading ? (
+            <Skeleton
+            width={500}
+            height={500}
+            animation="wave"/>
+        ) :(
+            <img 
+            src="https://source.unsplash.com/random/256*144"
+             alt="skeleton"
+            width={500}
+            height={500} />
+        )
+            
+        }
+
+        <Button variant='contained'
+        sx={{marginBottom:"10px"}}
+         onClick={()=> setLoading(false)} >Refresh to Open Skeleton</Button>
+
+        <Button variant='contained'
+         onClick={()=> setLoading(true)} >Refresh to Close Skeleton</Button>
     </Box>
   )
 }
