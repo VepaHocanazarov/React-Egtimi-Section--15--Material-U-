b0a66c65ec5f49f5c7a69665125a1814f3f2e32a
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-04 12:54:31 +0300

    180. Snackbar Kullanımı

--- src/Components/LessonSnackBar.tsx
+++ src/Components/LessonSnackBar.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import {Button,Snackbar,IconButton} from "@mui/material";
+import React, { useState } from 'react';
+import { Button, Snackbar, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
 export const LessonSnackBar = () => {
-  return (
-    <div>LessonSnackBar</div>
-  )
+
+    const [openSnackbar, setopenSnackbar] = useState(false);
+
+    const handleClose = () => {
+        setopenSnackbar(false);
+    }
+
+    const handleOpen = () => {
+
+        setopenSnackbar(true);
+
+    };
+
+    const action = (
+        <React.Fragment>
+
+            <Button onClick={handleClose} color='secondary' size='small' >Geri Al</Button>
+            <IconButton>
+                <CloseIcon onClick={handleClose} sx={{ color: "white" }} />
+            </IconButton>
+
+        </React.Fragment>
+    )
+    return (
+        <div>
+            <Button onClick={handleOpen}  >Snackbar'ı Aç</Button>
+            <Snackbar
+                message="Bir Hatta Oluştu"
+                open={openSnackbar}
+                action={action}
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                autoHideDuration={2000}
+                onClose={handleClose}
+            />
+        </div>
+    )
 }
 
