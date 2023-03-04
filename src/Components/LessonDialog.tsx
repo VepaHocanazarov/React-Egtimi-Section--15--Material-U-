cb5936b75ef42dc4662272aa70513dbad3da555b
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-04 11:20:59 +0300

    179. Dialog Kullanımı

--- src/Components/LessonDialog.tsx
+++ src/Components/LessonDialog.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import { Dialog,DialogContent,DialogContentText,DialogActions,Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Dialog, DialogContent, DialogContentText, DialogActions, Button, DialogTitle } from '@mui/material';
 
 export const LessonDialog = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  console.log(dialogOpen);
+  
   return (
-    <div>LessonDialog</div>
+
+    <>
+      <Button onClick={()=>setDialogOpen(true)} >Dialogu Aç</Button>
+      <Dialog open={dialogOpen} onClose={()=>setDialogOpen(false)} >
+
+        <DialogTitle>Hangisini Seviyorsun</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Frontend Frameworklerinden Hangisini Seviyorsunuz?</DialogContentText>
+          <DialogActions>
+            <Button onClick={()=>setDialogOpen(false)}>Angular</Button>
+            <Button onClick={()=>setDialogOpen(false)}>React Js</Button>
+          </DialogActions>
+        </DialogContent>
+
+      </Dialog>
+    </>
   )
 }
