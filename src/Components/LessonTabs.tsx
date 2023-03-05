55f003feb51b282f1b84256149438805b7055a64
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-05 21:06:06 +0300

    188. Tabs Kullanımı

--- src/Components/LessonTabs.tsx
+++ src/Components/LessonTabs.tsx
@@ -7,6 +7,16 @@ import TabPanel  from "@mui/joy/TabPanel";
 
 export const LessonTabs = () => {
   return (
-    <div>LessonTabs</div>
+    <Tabs defaultValue={0} >
+        <TabList>
+            <Tab>First</Tab>
+            <Tab>Second</Tab>
+            <Tab>Third</Tab>
+        </TabList>
+
+        <TabPanel value={0} >First </TabPanel>
+        <TabPanel value={1}>Second</TabPanel>
+        <TabPanel value={2}>Third</TabPanel>
+    </Tabs>
   )
 }
